37427fa83a4cf3de8b29a59c51ae6664b0967951
jenkj010 <83968728+jenkj010@users.noreply.github.com>
2026-07-04 19:55:18 +0100

    Fix Next.js build output directory

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
-import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-/** @type {(phase: string) => import('next').NextConfig} */
-export default function nextConfig(phase) {
-  return {
-    distDir: phase === PHASE_DEVELOPMENT_SERVER ? ".next-dev" : ".next-build",
-  };
-}
+export default nextConfig;
